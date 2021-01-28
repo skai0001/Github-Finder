8e6f99ab584b4aceedcf5b0580a2ef76047043b5
Hasan Skaiky <hasanskaiky@Hasans-MacBook-Air.local>
2021-01-27 22:07:53 -0500

    added github API to fetch the data

--- src/App.js
+++ src/App.js
@@ -2,17 +2,28 @@ import React, {Fragment, Component} from 'react'
 import Navbar from "./components/layout/Navbar";
 import UserItem from "./components/users/UserItem";
 import Users from "./components/users/Users";
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+    state = {
+        users:[],
+        loading:false
+    }
+
+    async componentDidMount() {
+        this.setState({loading:true});
+        const res = await axios.get('https://api.github.com/users');
+        console.log(res.data);
+        this.setState({users: res.data, loading:false})
+    }
 
     render() {
         return (
-
             <Fragment>
                 <Navbar/>
                 <div className='container'>
-                    <Users/>
+                    <Users loading={this.state.loading} users={this.state.users}/>
                 </div>
             </Fragment>
         )
