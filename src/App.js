59a79a3543464344ff67fc0e07b0da83da2e5930
Hasan Skaiky <hasanskaiky@Hasans-MacBook-Air.local>
2021-01-27 20:58:03 -0500

    section3- video #12

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, {Fragment, Component} from 'react'
-import './App.css';
 import Navbar from "./components/layout/Navbar";
+import UserItem from "./components/users/UserItem";
+import Users from "./components/users/Users";
+import './App.css';
+
 class App extends Component {
 
     render() {
@@ -8,6 +11,9 @@ class App extends Component {
 
             <Fragment>
                 <Navbar/>
+                <div className='container'>
+                    <Users/>
+                </div>
             </Fragment>
         )
     }
